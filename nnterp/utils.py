be66f26dead7d8a73a79332ca876d4d5e9fb5324
Clement Dumas <clement.dumas@ens-paris-saclay.fr>
2024-10-18 16:32:07 +0200

    changed tl error to class to avoid bugs in isinstance

--- nnterp/utils.py
+++ nnterp/utils.py
@@ -1,14 +1,15 @@
-def tl_import_error(e):
-    def raise_error(*args, **kwargs):
+class TLImportError:
+    def __init__(self, e):
+        self.e = e
+
+    def __call__(self, *args, **kwargs):
         print(
             "You're trying to use TransformerLens but you don't have it installed. The following error was caught when you imported nnterp:"
         )
-        raise e
-
-    return raise_error
+        raise self.e
 
 
 try:
     from nnsight.models.UnifiedTransformer import UnifiedTransformer
 except ImportError as e:
-    UnifiedTransformer = tl_import_error(e)
+    UnifiedTransformer = TLImportError(e)
