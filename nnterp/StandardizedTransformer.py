4945cb5a8b07331cbac4c0b79c1d74f1970324fc
Clement Dumas <butanium.contact@gmail.com>
2025-02-01 14:22:49 -0800

    wip

--- nnterp/StandardizedTransformer.py
+++ nnterp/StandardizedTransformer.py
@@ -4,50 +4,100 @@ import torch as th
 from nnsight_utils import LanguageModel, InterventionProxy, Envoy
 
 
-# Names to rename in the model
-attention_names = ["attn", "self_attention", "attention"]
-model_names = ["transformer", "gpt_neox"]
-layer_names = ["h"]
-ln_names = ["final_layer_norm", "ln_f"]
-lm_head_names = ["embed_out"]
+ATTENTION_NAMES = ["attn", "self_attention", "attention"]
+MODEL_NAMES = ["transformer", "gpt_neox"]
+LAYER_NAMES = ["h"]
+LN_NAMES = ["final_layer_norm", "ln_f"]
+LM_HEAD_NAMES = ["embed_out"]
 
 
-llm_rename_dict = {}
-for name in attention_names:
-    llm_rename_dict[name] = "self_attn"
-for name in model_names:
-    llm_rename_dict[name] = "model"
-for name in layer_names:
-    llm_rename_dict[name] = "layers"
-for name in ln_names:
-    llm_rename_dict[name] = "ln_final"
+def get_rename_dict(
+    attn_name: str | list[str] | None = None,
+    mlp_name: str | list[str] | None = None,
+    ln_final_name: str | list[str] | None = None,
+    lm_head_name: str | list[str] | None = None,
+) -> dict[str, str]:
+
+    rename_dict = (
+        {name: "self_attn" for name in ATTENTION_NAMES}
+        | {name: "model" for name in MODEL_NAMES}
+        | {name: "layers" for name in LAYER_NAMES}
+        | {name: "ln_final" for name in LN_NAMES}
+        | {name: "lm_head" for name in LM_HEAD_NAMES}
+    )
+
+    def update_rename_dict(renaming: str, value: str | list[str] | None):
+        if value is not None:
+            if isinstance(value, str):
+                rename_dict[value] = renaming
+            else:
+                for name in value:
+                    rename_dict[name] = renaming
+
+    update_rename_dict("self_attn", attn_name)
+    update_rename_dict("mlp", mlp_name)
+    update_rename_dict("ln_final", ln_final_name)
+    update_rename_dict("lm_head", lm_head_name)
+
+    return rename_dict
 
 
 class StandardizedTransformer(LanguageModel):
-    """StandardizedTransformer implementation for Hugging Face models."""
+    """
+
+    StandardizedTransformer implementation for Hugging Face models.
+    Renames the model modules to match the following architecture:
+    StandardizedTransformer
+        model
+            layers
+                self_attn
+                mlp
+            ln_final
+        lm_head
+    It already renames most models correctly, but you can add extra renaming rules.
+    It also contains built-in functions to e.g. get the layer input, output, etc.
+
+
+    Args:
+        model_name: Name of the model to load.
+        no_space_on_bos: If True
+        attn_name: Extra module names to rename to self_attn.
+        mlp_name: Extra module names to rename to mlp.
+        ln_final_name: Extra module names to rename to ln_final.
+        lm_head_name: Extra module names to rename to lm_head.
+    """
+
 
     def __init__(
         self,
         model_name: str,
-        no_space_on_bos: bool = False,
         trust_remote_code: bool = False,
+        attn_name: str | None = None,
+        mlp_name: str | None = None,
+        ln_final_name: str | None = None,
+        lm_head_name: str | None = None,
         **kwargs,
     ):
         kwargs.setdefault("torch_dtype", th.float16)
+
         kwargs.setdefault("device_map", "auto")
 
         tokenizer_kwargs = kwargs.pop("tokenizer_kwargs", {})
-        if no_space_on_bos:
-            tokenizer_kwargs.update(
-                dict(add_prefix_space=False, trust_remote_code=trust_remote_code)
-            )
         super().__init__(
             model_name,
             tokenizer_kwargs=tokenizer_kwargs,
             trust_remote_code=trust_remote_code,
-            rename_modules_dict=llm_rename_dict,
+            rename_modules_dict=get_rename_dict(
+                attn_name=attn_name,
+                mlp_name=mlp_name,
+                ln_final_name=ln_final_name,
+                lm_head_name=lm_head_name,
+            ),
             **kwargs,
         )
+        try:
+            model = self.model
+        except
 
     def get_num_layers(self) -> int:
         return len(self.model.layers)
